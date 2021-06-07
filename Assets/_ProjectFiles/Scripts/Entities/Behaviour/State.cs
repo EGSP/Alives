766cdc2ae03230d7d4d77c2715d07baad00d20ac
EveryGameSPlay <repsbro@gmail.com>
2021-06-07 23:57:19 +0300

    Rename.

---
+++ Assets/_ProjectFiles/Scripts/Entities/Behaviour/State.cs
@@ -0,0 +1,34 @@
+﻿namespace Alive.Behaviours
+{
+    /// <summary>
+    /// Состояние поведения. Другими словами вся логика.
+    /// </summary>
+    public abstract class BehaviourState<TEntity> where TEntity : BehaviourEntity<TEntity>
+    {
+        /// <summary>
+        /// Текущее поведение данного состояния.
+        /// </summary>
+        protected readonly Behaviour<TEntity> Behaviour;
+
+        protected BehaviourState(Behaviour<TEntity> behaviour)
+        {
+            Behaviour = behaviour;
+        }
+
+        public virtual void Awake()
+        {
+        }
+
+        public virtual void Update(in UpdateData u)
+        {
+        }
+
+        public virtual void AwakeVirtual()
+        {
+        }
+
+        public virtual void UpdateVirtual(in UpdateData u)
+        {
+        }
+    }
+}
---
+++ Assets/_ProjectFiles/Scripts/Entities/Mono/VisualShellMono.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Egsp.Core;
+using UnityEngine;
+using Component = Egsp.CSharp.Component;
+
+namespace Alive
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class EntityVisualShell : MonoBehaviour
+    {
+        public EventBus Bus { get; private set; } = new EventBus();
+
+        public WeakReference<Entity> Owner { get; set; }
+        private Entity _owner;
+        
+        public void LateUpdate()
+        {
+            // Virtual component is not initialized yet.
+            if (Owner == null)
+                return;
+            
+            if (Owner.TryGetTarget(out _owner) == false)
+            {
+                Debug.Log("No owner");
+                Destroy();
+            }
+        }
+
+        private void Destroy()
+        {
+            Destroy(gameObject);
+        }
+        
+        private void OnDestroy()
+        {
+            Bus.Raise<VisualShellComponent>(x=>x.OnShellDestroy());
+        }
+    }
+}
